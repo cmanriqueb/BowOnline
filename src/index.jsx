f497a224d1214c2945bf6f7a251888d7dd85e603
Carlos Manrique <carlosmanriqueb@yahoo.com>
2022-10-24 13:36:07 -0600

    update to index

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,6 @@ import ReactDOM from "react-dom/client";
 import { BowOnlineAPP } from "./BowOnlineAPP";
 import { BrowserRouter } from "react-router-dom";
 
-// ReactDOM.createRoot( document.getElementById('root')).render(
-//     <React.StrictMode>
-//         <BowOnlineAPP />
-//     </React.StrictMode>
-// )
-
 ReactDOM.createRoot( document.getElementById('root')).render(
     <BrowserRouter>
       <BowOnlineAPP />
